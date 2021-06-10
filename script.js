597ac44250e843087a277e34d5402e7473c4cfc3
Hexol123 <sebihangan123@gmail.com>
2021-06-10 16:43:46 +0400

    final project

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('Salut frate');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const TwitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+// Show New Quote
+function newQuote(){
+    loading();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check if Author field is bank and replace it with 'Unknow'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+    // Check Quote lenght to determine styling
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    // Set Quote , hide loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get Quotes From API
+async function getQuotes(){
+    loading();
+    const apiUrl =  'https://type.fit/api/quotes';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch(error){
+        //catch error here
+    }
+}
+
+// Tweet Quote
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+
+// Event listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+TwitterBtn.addEventListener('click',tweetQuote);
+
+//On Load
+getQuotes();
